0c1077c63af94a263b66e13180ff2b9b976c0de1
aaronestradabonilla777 <lacomarcaz@hotmail.com>
2024-01-06 11:51:08 -0600

    Add fizz_buzz function to main.py

--- pruebastecnicas002/src/main.py
+++ pruebastecnicas002/src/main.py
@@ -1,5 +1,15 @@
-def main():
-    print("¡Hola, mundo!")
 
-if __name__ == "__main__":
-    main()
+#def fizz_buzz(): es una funcion que imprime los numeros del 1 al 100,
+#pero para los multiplos de 3 imprime fizz, para los multiplos de 5 imprime buzz y para los multiplos de 3 y 5 imprime fizzbuzz
+def fizz_buzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+fizz_buzz()
